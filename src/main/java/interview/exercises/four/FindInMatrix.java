7619ebf647d02416379dc6383828026b76e56f14
McWen <wyx1478963@163.com>
2018-07-12 22:35:42 +0800

    从有序矩阵中寻找指定数字

---
+++ src/main/java/interview/exercises/four/FindInMatrix.java
@@ -0,0 +1,43 @@
+package interview.exercises.four;
+
+/**
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * <p>
+ * Consider the following matrix:
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * <p>
+ * Given target = 5, return true.
+ * Given target = 20, return false.
+ */
+public class FindInMatrix {
+
+    public static boolean findNum(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int rows = matrix.length, cols = matrix[0].length;
+        int r = 0, c = cols - 1; // 从右上角开始
+        while (r <= rows - 1 && c >= 0) {
+            if (target == matrix[r][c])
+                return true;
+                //大于就向下
+            else if (target > matrix[r][c])
+                r++;
+                // 小于就向左
+            else
+                c--;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        System.out.println(findNum(array, 2));
+
+    }
+}
