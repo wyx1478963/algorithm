904e62212888b40484656ded7f06f2b1b4bbb1df
McWen <wyx1478963@163.com>
2018-07-28 17:57:29 +0800

    约瑟夫环;

---
+++ src/main/java/interview/exercises/e62/Josephus.java
@@ -0,0 +1,14 @@
+package interview.exercises.e62;
+
+import java.util.Deque;
+
+/**
+ * Josephus
+ */
+public class Josephus {
+
+    public static int lastBoy(int[] nums) {
+       //循环队列
+        return 0;
+    }
+}
